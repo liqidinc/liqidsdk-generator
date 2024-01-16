640586dedd50d51c7c677c861b6f0734398887d4
Kurt Duncan <kurt.duncan@liqid.com>
2024-01-16 15:56:32 -0700

    Versioning

--- src/com/liqid/sdk/generator/Main.java
+++ src/com/liqid/sdk/generator/Main.java
@@ -19,7 +19,7 @@ import java.io.IOException;
 
 public class Main {
 
-    public static final String VERSION = "3.3.0";
+    public static final String VERSION = "3.4";
 
     private static final CodeWriter[] CODE_WRITERS = {
         new GoCodeWriter(),
